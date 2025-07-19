c7c569c129a5937058b773311808df39e5612330
Gr0sCrane <Mouadchd05@gmail.com>
2025-07-19 19:55:54 +0200

    moved struct position

--- headers/classes.hpp
+++ headers/classes.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <string>
 #include <functional>
+#include "position.hpp"
 
 /**
  * @enum EntityType
@@ -15,32 +16,6 @@ enum class EntityType {
     ITEM,
     VOID
 };
-
-/**
- * @struct Position
- * @brief Represent a position on the board.
- * @param x X coord
- * @param y Y coord
- */
-struct Position {
-    int x;
-    int y;
-
-    Position(int x = 0,int y = 0) : x(x), y(y) {}
-
-    bool operator==(const Position& other) const {
-        return x == other.x && y == other.y;
-    }
-
-};
-namespace std {
-    template <>
-    struct hash<Position> {
-        std::size_t operator()(const Position& pos) const {
-            return std::hash<int>()(pos.x) ^ (std::hash<int>()(pos.y) << 1);
-        }
-    };
-}
 
 class Entity {
     private:
